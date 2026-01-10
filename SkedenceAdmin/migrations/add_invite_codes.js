67a4108443de2d3d85861a4333500a79665fcdfc
mttsprague <mttsprague@gmail.com>
2026-01-10 00:48:47 -0600

    fix: Update migration script to use service account credentials
    Allows add_invite_codes.js to run without environment variable setup

--- SkedenceAdmin/migrations/add_invite_codes.js
+++ SkedenceAdmin/migrations/add_invite_codes.js
@@ -4,10 +4,11 @@
  */
 
 const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
 
 // Initialize Firebase Admin
 admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
