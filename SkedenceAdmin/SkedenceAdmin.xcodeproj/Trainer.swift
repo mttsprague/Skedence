1c7d4c75796c02f9cc43bf99134aaccc14174e4c
mttsprague <mttsprague@gmail.com>
2026-01-11 13:56:51 -0600

    fix: manually implement Codable for Trainer to exclude id field
    - Add custom init(from:) that only decodes fields in CodingKeys
    - Add custom encode(to:) that only encodes fields in CodingKeys
    - This prevents Codable from trying to decode/encode the id field
    - id will be set manually from Firestore document ID

--- SkedenceAdmin/SkedenceAdmin.xcodeproj/Trainer.swift
+++ SkedenceAdmin/SkedenceAdmin.xcodeproj/Trainer.swift
@@ -32,4 +32,32 @@ struct Trainer: Identifiable, Codable, Hashable {
     enum CodingKeys: String, CodingKey {
         case firstName, lastName, email, avatarUrl, photoURL, imageUrl, orgId, name
     }
+    
+    // Custom init to exclude id from decoding
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        firstName = try container.decodeIfPresent(String.self, forKey: .firstName)
+        lastName = try container.decodeIfPresent(String.self, forKey: .lastName)
+        email = try container.decodeIfPresent(String.self, forKey: .email)
+        avatarUrl = try container.decodeIfPresent(String.self, forKey: .avatarUrl)
+        photoURL = try container.decodeIfPresent(String.self, forKey: .photoURL)
+        imageUrl = try container.decodeIfPresent(String.self, forKey: .imageUrl)
+        orgId = try container.decodeIfPresent(String.self, forKey: .orgId)
+        name = try container.decodeIfPresent(String.self, forKey: .name)
+        // id is not decoded - will be set manually from document ID
+    }
+    
+    // Custom encode to exclude id from encoding
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeIfPresent(firstName, forKey: .firstName)
+        try container.encodeIfPresent(lastName, forKey: .lastName)
+        try container.encodeIfPresent(email, forKey: .email)
+        try container.encodeIfPresent(avatarUrl, forKey: .avatarUrl)
+        try container.encodeIfPresent(photoURL, forKey: .photoURL)
+        try container.encodeIfPresent(imageUrl, forKey: .imageUrl)
+        try container.encodeIfPresent(orgId, forKey: .orgId)
+        try container.encodeIfPresent(name, forKey: .name)
+        // id is not encoded
+    }
 }
