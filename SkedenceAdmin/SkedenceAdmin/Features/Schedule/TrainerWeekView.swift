c46065f3566d52b6630c4241545028fcda8624de
mttsprague <mttsprague@gmail.com>
2026-02-07 22:15:36 -0600

    Fix booking query field name from clientId to clientUID
    - Cloud function saves booking with clientUID field
    - Swift code was querying with clientId field (mismatch)
    - This caused booking documents to not be found
    - Fixed query in TrainerWeekView and AllTrainersDayView

--- SkedenceAdmin/SkedenceAdmin/Features/Schedule/TrainerWeekView.swift
+++ SkedenceAdmin/SkedenceAdmin/Features/Schedule/TrainerWeekView.swift
@@ -405,7 +405,7 @@ struct TrainerWeekView: View {
                 do {
                     let db = Firestore.firestore()
                     let bookingsSnapshot = try await db.collection("bookings")
-                        .whereField("clientId", isEqualTo: clientId)
+                        .whereField("clientUID", isEqualTo: clientId)
                         .whereField("trainerId", isEqualTo: slot.trainerId)
                         .whereField("startTime", isEqualTo: Timestamp(date: slot.startTime))
                         .limit(to: 1)
